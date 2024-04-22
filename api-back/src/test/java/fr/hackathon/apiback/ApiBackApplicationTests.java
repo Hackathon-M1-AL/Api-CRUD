397e954f54f09f857a7326b6b35af92ada7be37a
Matthieu Flce <flacelierematthieu@gmail.com>
2024-04-22 18:00:36 +0200

    feat: add dev-prod env

--- api-back/src/test/java/fr/hackathon/apiback/ApiBackApplicationTests.java
+++ api-back/src/test/java/fr/hackathon/apiback/ApiBackApplicationTests.java
@@ -2,8 +2,10 @@ package fr.hackathon.apiback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ApiBackApplicationTests {
 
     @Test
