3048572100205df5d69e557a5ebde2b91b60f716
GALLIC-A <axel.gallic@orange.fr>
2024-04-22 17:47:27 +0200

    Ajout du OutProduitDto pour les produits

---
+++ api-back/src/main/java/fr/hackathon/apiback/application/dto/OutProduitDto.java
@@ -0,0 +1,15 @@
+package fr.hackathon.apiback.application.dto;
+
+import fr.hackathon.apiback.infrastructure.entity.Catalogue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutProduitDto {
+    private Long id;
+    private String nom;
+    private String description;
+    private int quantite;
+    private Double Prix;
+    private List<Catalogue> catalogues = new ArrayList<>();
+}
