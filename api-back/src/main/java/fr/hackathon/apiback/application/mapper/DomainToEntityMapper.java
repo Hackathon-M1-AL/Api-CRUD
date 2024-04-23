c602c43802da7fc75cac9dd2b291ad9ef46699b6
GALLIC-A <axel.gallic@orange.fr>
2024-04-23 11:08:49 +0200

    Correction de DtoToDomainMapper et ajout de DomainToEntityMapper

--- api-back/src/main/java/fr/hackathon/apiback/application/mapper/DomainToEntityMapper.java
+++ api-back/src/main/java/fr/hackathon/apiback/application/mapper/DomainToEntityMapper.java
@@ -1,15 +1,12 @@
 package fr.hackathon.apiback.application.mapper;
 
 import fr.hackathon.apiback.application.dto.produit.InProduitDto;
-import fr.hackathon.apiback.application.dto.produit.OutProduitDto;
 import fr.hackathon.apiback.domain.ports.Produit;
 import fr.hackathon.apiback.infrastructure.entity.ProduitEntity;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
-public interface DtoToDomainMapper {
-    Produit inDtoToDomain(final InProduitDto inDto);
-    OutProduitDto domainToOutDto(final Produit domain);
+public interface DomainToEntityMapper {
+    ProduitEntity domainToEntity(final Produit domain);
+    Produit entityToDomain(final ProduitEntity entity);
 }
